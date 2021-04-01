c24a33fc6382228ce69ac359dc9b5a66e033291d
James <jamesmca@live.com>
2021-03-31 19:17:21 -0700

    modal is functioning :)
    added modal styles, esc key functionality and offclick functionality and used styledcomponents for the first time

--- src/App.js
+++ src/App.js
@@ -1,10 +1,40 @@
-import React from 'react'
+import React, { useState } from 'react'
+import styled from 'styled-components'
+import Modal from './components/Modal'
+import { GlobalStyle } from './globalStyles'
 
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh
+`
+
+const Button = styled.button`
+  mind-width: 100px;
+  padding: 16px 32px;
+  border-radius: 4px;
+  border: none;
+  background: #141414;
+  color: #fff;
+  font-size: 24px;
+  cursor: pointer;
+`
 
 function App() {
+  const [showModal, setShowModal] = useState(false)
+
+  function openModal() {
+    setShowModal(prev => !prev)
+  }
+
   return (
     <>
-    <h1>what uppp</h1>
+      <Container>
+        <Button onClick={openModal}>I am a modal</Button>
+        <Modal showModal={showModal} setShowModal={setShowModal}/>
+        <GlobalStyle />
+      </Container>
     </>
   );
 }
